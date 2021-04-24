0dc121c3c8b76c3730c4c6dd1f249357aa0c4b09
manavkaushal26 <manav.kaushal@spacejoy.com>
2021-04-24 20:13:42 +0530

    First commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBk_E2a47J1b0isnBHNbBif6a4G6Lpej-8",
+  authDomain: "slack-clone-782ab.firebaseapp.com",
+  projectId: "slack-clone-782ab",
+  storageBucket: "slack-clone-782ab.appspot.com",
+  messagingSenderId: "953036045598",
+  appId: "1:953036045598:web:55c158c22100cf67a99d0f",
+};
+
+export default firebaseConfig;
