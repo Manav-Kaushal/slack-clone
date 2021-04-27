ac37709946e25291f1ab79edec463846c20e242c
manavkaushal26 <manav.kaushal@spacejoy.com>
2021-04-28 02:48:33 +0530

    Left sidebar complete (firebase hooks & REDUX)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import styled from "styled-components";
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+`;
 
 function App() {
   return (
     <div className="app">
       <Router>
         <>
-          {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-          <Switch>
-            <Route path="/" exact>
-              <Header />
-            </Route>
-          </Switch>
+          <Header />
+          <AppBody>
+            <Sidebar />
+            <Switch>
+              <Route path="/" exact>
+                {/* Chat Component */}
+              </Route>
+            </Switch>
+          </AppBody>
         </>
       </Router>
     </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBk_E2a47J1b0isnBHNbBif6a4G6Lpej-8",
   authDomain: "slack-clone-782ab.firebaseapp.com",
@@ -7,4 +9,9 @@ const firebaseConfig = {
   appId: "1:953036045598:web:55c158c22100cf67a99d0f",
 };
 
-export default firebaseConfig;
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, provider };
